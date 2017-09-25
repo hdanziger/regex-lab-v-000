78473c476fe050f441de8d6f8e0c38f292c5d7ce
Hannah Danziger <hdanziger8@gmail.com>
2017-09-25 18:41:20 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/^[A-Z].+\p{P}\b/)
+  text.scan(/^[A-Z].+\p{P}\b/) ? true : false
 
 end
 
